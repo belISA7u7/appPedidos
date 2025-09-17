ad416e5ebd646ae361d24da3a50e91deab993c2a
isa_bel <bel494621@gmail.com>
2025-09-16 20:30:14 -0400

    se agrego el dbset para cada modelo y se actualizo el program.cs

--- appPedidos/Data/ApplicationDbContext.cs
+++ appPedidos/Data/ApplicationDbContext.cs
@@ -1,6 +1,20 @@
-﻿namespace appPedidos.Data
+﻿using appPedidos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace appPedidos.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        // Constructor que llama al base con las opciones
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet para cada modelo (tabla)
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
